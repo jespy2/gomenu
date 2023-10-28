585b1d7eb4ade752626c70c209859a441bce4349
jespy2 <jespy2@gmail.com>
2023-10-28 11:33:47 -0600

    updated routing after adding new recipe

--- client/src/forms/recipe-form/RecipeForm.tsx
+++ client/src/forms/recipe-form/RecipeForm.tsx
@@ -64,7 +64,7 @@ export const RecipeForm = (props: IProps) => {
 			})
 			.then(() => setRecipe({...values}))
 			.then(() => setNewRecipe({...values}))
-			.then(() => navigate('/showrecipe'))
+			.then(() => navigate('/cookbook'))
 	}
 
 	const handleNextStep = () => {
